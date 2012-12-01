64bb4c9f196fc8d84bca590ea6beddfdc297adc7
Luke Reeves <luke@neuro-tech.net>
2012-12-01 04:09:28 +0000

    Switch everything over to routed instead of bridged, dur

--- files/conf/openvpn/down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BR=$1
-DEV=$2
-#
-/sbin/brctl delif $BR $DEV
-/sbin/ifconfig $DEV down
-
--- files/conf/openvpn/up.sh
+++ files/conf/openvpn/up.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-#
-BR=$1
-DEV=$2
-MTU=$3
-/sbin/ifconfig $DEV mtu $MTU promisc up
-/sbin/brctl addif $BR $DEV
+
+/sbin/iptables -F
+/sbin/iptables -F -t nat
+/sbin/iptables -A FORWARD -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -j MASQUERADE
+
+
+
